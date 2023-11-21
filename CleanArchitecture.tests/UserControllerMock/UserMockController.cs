6f1d2d963facffb6e6491f31278500785fdb3738
Paulo Cesar C Bruno <p_bruno001@hotmail.com>
2023-11-21 02:50:11 -0300

    Add. Inicio dos testes unitários

---
+++ CleanArchitecture.tests/UserControllerMock/UserMockController.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using CleanArchitecture.Application.ResponseBase;
+using CleanArchitecture.Domain.Interfaces;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.tests.UserControllerMock;
+
+public class UserMockController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+
+    public UserMockController(IMediator mediator, IUserRepository userRepository,
+                  IMapper mapper)
+    {
+        _mediator = mediator;
+        _userRepository = userRepository;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public ActionResult<List<Response>> GetAll()
+    {
+        return Ok(Response);
+    }
+
+    [HttpGet("badrequest")]
+    public ActionResult<List<Response>> GetAllBadRequest()
+    {
+        try
+        {
+            throw new Exception();
+        }
+        catch (Exception)
+        {
+            return BadRequest();
+        }
+    }
+}
+
+// AINDA NÃO TERMINEI OS TESTES UNITÁRIOS => PROCURANDO TEMPO....
---
+++ CleanArchitecture.tests/UserUnitTestController.cs
@@ -0,0 +1,90 @@
+﻿using AutoMapper;
+using CleanArchitecture.Application.ProfileMapper;
+using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Persistence.Context;
+using CleanArchitecture.Persistence.Repositories;
+using CleanArchitecture.tests.UserControllerMock;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.tests;
+
+public class UserUnitTestController
+{
+    private IMapper mapper;
+    private IUserRepository repository;
+
+    public static DbContextOptions<AppDbContext> dbContextOptions { get; }
+
+    public static string connectionString = "Data Source=userdb.db";
+          
+    static UserUnitTestController()
+    {
+        dbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+             .UseSqlite(connectionString)
+             .Options;
+    }
+          
+    public UserUnitTestController()
+    {
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile(new GetAllUserMapper());
+        });
+
+        // Instancia do AutoMapper:
+        mapper = config.CreateMapper();
+
+        // Meu Contexto.
+        var context = new AppDbContext(dbContextOptions);
+
+        // Se eu fosse usar a Classe de Mock (DBUnitTestsMockInitialize).
+        // DBUnitTestsMockInitialize db = new DBUnitTestsMockInitialize();
+        // db.Seed(context);
+
+        // Como estou usando meu banco e Contexto Original, fica:
+        // Instancia do Repository...
+        repository = new UserRepository(context);
+
+    }
+
+    // Inicio dos testes unitários:
+    // Casos de teste:               
+    // TESTES DE CONSULTAS
+    //===================================================================================================== 
+
+    /* Testar o método GET do Controlador de UserMockController:
+       Testar se o valor retornado é igual a um OkObjectResult 
+       é igual ao estatus code 200(Ok).*/
+    [Fact]  // Informa que é um código de teste unitário.  
+    public void GetUser_Return_OkResult_200_OK()
+    {
+        //Arrange  
+        var controller = new UserMockController(null, repository, mapper);
+
+
+        //Act
+        var data = controller.GetAll();
+
+        //Assert         
+        Assert.IsType<OkObjectResult>(data.Result);
+    }
+    /* Testar o método GET do Controlador de UserMockController:
+           Testar se o valor retornado é igual a um OkObjectResult 
+           é igual ao estatus code 400(BadRequest).*/
+    [Fact]  // Informa que é um código de teste unitário.  
+    public void GetUser_Return_BadRequest_400_BadRequest()
+    {
+        //Arrange  
+        var controller = new UserMockController(null, repository, mapper);
+
+
+        //Act
+        var data = controller.GetAllBadRequest();
+
+        //Assert        
+        Assert.IsType<BadRequestResult>(data.Result);
+    }
+
+    // AINDA NÃO TERMINEI OS TESTES UNITÁRIOS => PROCURANDO TEMPO....
+}
